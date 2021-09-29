4118e9d15baaf7b26c2a57738adfe1cf3a67177a
Shubham <svdixit8388@gmail.com>
2021-09-29 12:13:46 +0530

    asc and desc sorting added

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,47 +1,65 @@
+import React from "react";
 import Like from "./like";
 
-const Table = (props) => {
-  const { items, onLikeClick, onDelete, tableHeaderTitles, tableKeys } = props;
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          {tableHeaderTitles.map((title, index) => (
-            <th key={index} scope="col">
-              {title}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => {
-          return (
-            <tr key={item.title}>
-              {tableKeys.map((key) => (
-                <th key={key}>{item[key]}</th>
-              ))}
-              <td>{item.genre.name}</td>
-              <td>
-                <Like
-                  liked={item.liked}
-                  onLikeClick={onLikeClick}
-                  movie={item}
-                />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => onDelete(item)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
+class Table extends React.Component {
+  raiseSort = (sortBy) => {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.sortBy === sortBy) {
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    } else {
+      sortColumn.sortBy = sortBy;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+  render() {
+    const { items, onLikeClick, onDelete, tableHeaderTitles, tableKeys } =
+      this.props;
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            {tableHeaderTitles.map((title, index) => (
+              <th
+                key={index}
+                scope="col"
+                onClick={() => this.raiseSort(title.key)}
+              >
+                {title.value}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => {
+            return (
+              <tr key={item.title}>
+                {tableKeys.map((key) => (
+                  <th key={key}>{item[key]}</th>
+                ))}
+                <td>{item.genre.name}</td>
+                <td>
+                  <Like
+                    liked={item.liked}
+                    onLikeClick={onLikeClick}
+                    movie={item}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDelete(item)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default Table;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import SidebarTabs from "./common/sidebar-tabs";
 import Table from "./common/table";
+import _ from "lodash";
 
 class Movies extends Component {
   state = {
@@ -13,11 +14,16 @@ class Movies extends Component {
     pageSize: 4,
     currentPageNumber: 1,
     selectedGenre: null,
+    sortColumn: { sortBy: "title", order: "asc" },
   };
 
   componentDidMount() {
     const genres = [{ name: "All Genres", _id: -999 }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres, selectedGenre: genres[0] });
+    this.setState({
+      movies: getMovies(),
+      genres,
+      selectedGenre: genres[0],
+    });
   }
 
   render() {
@@ -27,16 +33,25 @@ class Movies extends Component {
       pageSize,
       genres,
       selectedGenre,
+      sortColumn,
     } = this.state;
 
-    const movieTableHeaderTitle = ["Title", "Stock", "Rate", "Genre", "", ""];
+    const movieTableHeaderTitles = [
+      { key: "title", value: "Title" },
+      { key: "numberInStock", value: "Stock" },
+      { key: "dailyRentalRate", value: "Rate" },
+      { key: "genre.name", value: "Genre" },
+      { key: "", value: "" },
+      { key: "", value: "" },
+    ];
     const movieTableKeys = ["title", "numberInStock", "dailyRentalRate"];
 
     const filteredMovies =
       selectedGenre && selectedGenre._id !== -999
         ? allMovies.filter((movie) => movie.genre._id === selectedGenre._id)
         : allMovies;
-    const movies = paginate(filteredMovies, currentPageNumber, pageSize);
+    let movies = _.orderBy(filteredMovies, sortColumn.sortBy, sortColumn.order);
+    movies = paginate(movies, currentPageNumber, pageSize);
 
     if (!this.state.movies.length) {
       return <h1 className="m-4">There are no movies in the database.</h1>;
@@ -56,11 +71,13 @@ class Movies extends Component {
               Showing {filteredMovies.length} movies in the database.
             </h1>
             <Table
-              tableHeaderTitles={movieTableHeaderTitle}
+              tableHeaderTitles={movieTableHeaderTitles}
               tableKeys={movieTableKeys}
               items={movies}
               onLikeClick={this.handleLikeClick}
-              onDelete={this.deleteMovie}
+              onDelete={this.handleDeleteMovie}
+              onSort={this.handleSort}
+              sortColumn={sortColumn}
             />
             <Pagination
               itemsCount={filteredMovies.length}
@@ -95,17 +112,11 @@ class Movies extends Component {
     console.log(movies[updateMovieIndex]);
   };
 
-  getMovieCount() {
-    return (
-      this.state.movies.length && (
-        <h1 className="m-4">
-          Showing {this.state.movies.length} movies in the database.
-        </h1>
-      )
-    );
-  }
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
 
-  deleteMovie = (selectedMovie) => {
+  handleDeleteMovie = (selectedMovie) => {
     console.log(selectedMovie);
     this.setState({
       movies: this.state.movies.filter(
